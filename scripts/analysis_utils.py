1c567387dabdd2ee6ff64412dc88d2ac25886d0e
Rey Squillace <Reynier Squillace reynier@virginia.edu>
2026-02-23 13:31:58 -0500

    analysis scripts

---
+++ scripts/analysis_utils.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 14})
+
+def get_density2d(full_data, col1, col2, bins1, bins2, type = 'retrieved'):
+
+    bin_edges = [bins1, bins2]
+
+    cols = [col1, col2]
+
+    injected_density, _ = np.histogramdd(full_data[:, cols], bins=bin_edges)
+
+    if type == 'retrieved':
+        retrieved_mask = full_data[:, -1] > 0.
+        retrieved_density, _ = np.histogramdd(full_data[retrieved_mask][:, cols], 
+                                              bins=bin_edges)
+
+        return injected_density, retrieved_density
+    
+    elif type == 'missed':
+        sigma_mask = full_data[:, 8] > 6. #threshold for candidates 
+        missed_mask = full_data[:, -1] < 0. 
+        should_have_detected = full_data[sigma_mask & missed_mask]
+        missed_density, _ = np.histogramdd(should_have_detected[:, cols], 
+                                              bins=bin_edges)
+        return injected_density, missed_density
+
+def make_levels(arr, logspace = False, Nlevels = 20):
+
+    if logspace:
+        return np.logspace(np.log10(arr[arr > 0].min()), np.log10(arr.max()), Nlevels)
+    
+    else:
+        return np.linspace(0, 1, Nlevels)
+
+def add_colorbar(fig, ax, cf, tick_locs):
+    cbar = fig.colorbar(cf, ax=ax, location='right', pad=0.02)
+    cbar.set_label('Probability', labelpad=10)
+    cbar.set_ticks(tick_locs)
+    return cbar
+
+def add_crosshair(ax, x_bins, y_bins, x_dev, y_dev, x_log=False, y_log=False):
+
+    x_vals = x_bins[:-1]
+    y_vals = y_bins[:-1]
+
+    if x_log:
+        log_x = np.log(x_vals)
+        x_center = np.exp(log_x[-1] - 0.12 * (log_x[-1] - log_x[0]))
+        x_half_log = x_dev * (log_x[-1] - log_x[0]) / len(x_vals)
+        x_lo = np.exp(np.log(x_center) - x_half_log)
+        x_hi = np.exp(np.log(x_center) + x_half_log)
+    else:
+        x_center = x_vals[-1] - 0.12 * (x_vals[-1] - x_vals[0])
+        x_half = x_dev * (x_vals[-1] - x_vals[0]) / len(x_vals)
+        x_lo = x_center - x_half
+        x_hi = x_center + x_half
+
+    if y_log:
+        log_y = np.log(y_vals)
+        y_center = np.exp(log_y[-1] - 0.12 * (log_y[-1] - log_y[0]))
+        y_half_log = y_dev * (log_y[-1] - log_y[0]) / len(y_vals)
+        y_lo = np.exp(np.log(y_center) - y_half_log)
+        y_hi = np.exp(np.log(y_center) + y_half_log)
+    else:
+        y_center = y_vals[-1] - 0.12 * (y_vals[-1] - y_vals[0])
+        y_half = y_dev * (y_vals[-1] - y_vals[0]) / len(y_vals)
+        y_lo = y_center - y_half
+        y_hi = y_center + y_half
+
+    kw = dict(color='white', linewidth=1.5, transform=ax.transData)
+    ax.plot([x_lo, x_hi], [y_center, y_center], **kw)
+    ax.plot([x_center, x_center], [y_lo, y_hi], **kw)
+    ax.plot(x_center, y_center, 'w+', markersize=4)
+
+
