1666fc36eb7f9b0f66d98dd229c01e7c9fd8bc0b
Reynier Squillace <reynier@narval2.narval.calcul.quebec>
2026-07-04 14:35:37 -0400

    something wrong with yaml files

--- scripts/run_stack_injections.py
+++ scripts/run_stack_injections.py
@@ -89,20 +89,15 @@ def get_injections(N, maxdm, rng=None):
     
     return prof_idx, f, dm, S
 
-def call_no_retrieve(pointing, ii, prof_idx, f, dm, S):
-    ra = np.round(pointing[0], 2)
-    dec = np.round(pointing[1], 2)
-    sub_pointing = 0
+def call_no_retrieve(pspec_file, ii, prof_idx, f, dm, S):
+    ra = float(pspec_file.split('_')[0])
+    dec = float(pspec_file.split('_')[1])
     period_string = "_".join([f"{f[i]:.2f}" for i in ii])
     instant_time = time.time()
     temp_path = f'{transmissivity_repo_path}/scripts/inj_{instant_time}_{period_string}_{ra}_{dec}.yaml'
     make_fake.make_yaml(len(ii), temp_path, 'tpa', None, prof_idx[ii], f[ii], dm[ii], S[ii])
     print('Made fakes.')
 
-    #cand_path = f"{cand_directory_path}/injections/{ra}_{dec}_power_spectra_stack_20251128_21_{temp_path.split('/')[-1]}_()_candidates.npz"
-    #print(f'Trying to find candidates at {cand_path}.')
-    #cand_path = f"{cand_directory_path}/injections/{ra:.2f}_{dec:.2f}_{sub_pointing}_{temp_path.split('/')[-1]}_{str(range(len(ii))).replace(' ', '')}_{period_string}_injection_candidates.npz"
-    #print(f'cand path: {cand_path}')
     print('Attempting to inject.')
     if dec < 0:
         dec_string = f'" -{str(np.abs(dec))}"'
@@ -111,59 +106,12 @@ def call_no_retrieve(pointing, ii, prof_idx, f, dm, S):
     print('Running call.') 
 
     #---NARVAL TWEAKS---#
-    power_spectra_path = f'/project/ctb-vkaspi/champss/stack_202511/{ra}_{dec}_power_spectra_stack.hdf5'
+    power_spectra_path = f'/project/ctb-vkaspi/champss/stack_variable_depth/saved_stacks/{pspec_file}'
+    #power_spectra_path = f'/project/ctb-vkaspi/champss/stack_202511/{ra}_{dec}_power_spectra_stack.hdf5'
     #---#
 
     os.system(f'run-stack-search-pipeline {str(ra)} {dec_string} search-monthly --cand-path {cand_directory_path} \
             --file {power_spectra_path} --injection-path {temp_path} --only-injections')
-    #try:
-    #    cands = np.load(cand_path, allow_pickle = True)
-    #    print('Loaded candidate file.')
-    #except:
-    #    print('Could not load candidate file.')
-
-    #output = np.zeros((len(ii), 14))
-    #date of 0 represents stack
-    #output[:, 0:3] = np.array([ra, dec, 0])[np.newaxis, :]
-
-    #cand_freqs = []
-    #cand_idx = []
-    #for i in range(cands['cand_count'].item()):
-    #    cand = cands[f'candidate_{i}'].item()
-    #    cand_freqs.append(cand['freq'])
-    #    if cand['injection']:
-    #        cand_idx.append(cand['injection_dict']['injection_index'])
-    #    else:
-    #        cand_idx.append(-1)
-
-    #cand_freqs = np.asarray(cand_freqs)
-    #cand_idx = np.asarray(cand_idx)
-    #if len(cand_freqs) > 0:
-    #    print(f'Candidate frequencies are {[np.round(i, 3) for i in cand_freqs]} Hz.')
-    
-    #for i in range(len(ii)):
-    #    inj = cands['injection_dicts'][i]
-        #note that fwhm is totally borked in the output file
-        #arbitrarily setting it to 0.1
-        #this is okay, we can recover it from the TPA_idx
-    #    output[i, 3:10] = (prof_idx[ii[i]], inj['frequency'], inj['DM'], inj['flux'], 0.1, inj['predicted_sigma'], inj['predicted_nharm'])
-        
-    #    if len(cand_freqs) > 0:
-    #        same_injection = np.where(cand_idx == i)[0]
-    #        closest_match = np.argmin(np.abs(cand_freqs - f[ii[i]])) 
-    #        #if np.abs(cand_freqs[closest_match] - f[ii[i]]) <= 0.01: 
-    #        if closest_match in same_injection:
-    #            cand = cands[f'candidate_{closest_match}'].item()
-    #            output[i, 10:14] = (cand['sigma'], cand['features'].item()[3], cand['freq'], cand['dm'])
-    #            print(f'Matched candidate with frequency {cand_freqs[closest_match]:.3f} to injection with frequency {f[ii[i]]:.3f}.')
-    #        else:
-    #            output[i, 10:14] = -1 * np.ones(4)
-    #            print(f'There may be something concerning going on with f = {f[ii[i]]:.3f}, DM = {dm[ii[i]]:.2f}, and S = {S[ii[i]]:.2f}.')
-
-    #    else:
-    #        output[i, 10:14] = -1 * np.ones(4)
-    #        print(f'Did not find a match for injection with f = {f[ii[i]]:.3f}, DM = {dm[ii[i]]:.2f}, and S = {S[ii[i]]:.2f}.')
-    
     #output file has columns:
     #RA, Dec, Date, TPA_idx, f, DM, flux, fwhm, predicted_sigma, predicted_nharm, output_sigma, output_nharm, output_f, output_dm
     
@@ -174,13 +122,13 @@ def call_no_retrieve(pointing, ii, prof_idx, f, dm, S):
 def inject(N, pointing_idx, seed):
     rng = np.random.default_rng(seed)
     for idx in range(pointing_idx, pointing_idx + 50):
-        pointing = pointings[idx]
-        ra = pointing[0]
-        dec = pointing[1]
+        pspec_file = pspec_files[idx]
+        ra = float(pspec_file.split('_')[0])
+        dec = float(pspec_file.split('_')[1])
         #mode = 'database'
         #ap = find_closest_pointing(ra, dec, mode=mode)   
-        ra = np.round(ra, 2)
-        dec = np.round(dec, 2)
+        #ra = np.round(ra, 2)
+        #dec = np.round(dec, 2)
         print(f'Injecting {N} pulsars into ({ra}, {dec}).')
         maxdm = get_max_dm(ra, dec)
         #ensure randomization between workers
@@ -193,7 +141,7 @@ def inject(N, pointing_idx, seed):
 
         try:
             #all run in block in same call        
-            call_no_retrieve(pointing, main_injection_idx, prof_idx, f, dm, S)
+            call_no_retrieve(pspec_file, main_injection_idx, prof_idx, f, dm, S)
         except Exception as e:
             print(f"An error occurred: {e}")
             traceback.print_exc() 
@@ -215,11 +163,12 @@ transmissivity_repo_path = '/project/ctb-vkaspi/champss/injections/Transmissivit
 #---#
 with open('stack_maxdm.json', 'r') as f:
     maxdm_dict = json.load(f)
-pointings = np.load(f'{transmissivity_repo_path}/scripts/stack_pointings.npy')
-pointings = pointings
-print('Loaded pointings.')
-
-batch_indices = np.arange(len(pointings))[::50]  #start idx of each batch
+#pointings = np.load(f'{transmissivity_repo_path}/scripts/stack_pointings.npy')
+#pointings = pointings
+#print('Loaded pointings.')
+with open(f'{transmissivity_repo_path}/scripts/stack_var_files.txt', 'r') as f:
+    pspec_files = f.readlines()
+batch_indices = np.arange(len(pspec_files))[::50]  #start idx of each batch
 seeds = np.random.SeedSequence(42).spawn(len(batch_indices))
 
 task_id = int(os.environ["SLURM_ARRAY_TASK_ID"])  #range over len(batch_indices)
